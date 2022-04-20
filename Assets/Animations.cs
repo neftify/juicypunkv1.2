e68030a5b528d895c17b8016cf657e01cd45a1cd
Emanuel Acosta <emanuelacosta809@gmail.com>
2022-04-20 13:28:29 -0400

    extra prefabs

---
+++ Assets/Animations.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.InfiniteRunnerEngine;
+public class Animations : MonoBehaviour
+{
+    Animator animator;
+    PlayableCharacter player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animator.SetBool("Jumping", player.);
+    }
+}
--- Assets/LogoFadeOut.cs
+++ Assets/LogoFadeOut.cs
@@ -1,8 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using MoreMountains.InfiniteRunnerEngine;
 public class LogoFadeOut : MonoBehaviour
 {
-   
+    [SerializeField]
+    public Image logo;
+    [SerializeField]
+    public GameManager gm;
+    public float lifeTime = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    float a = 1;
+    // Update is called once per frame
+    void Update()
+    {
+       if ( gm.Status == GameManager.GameStatus.GameInProgress )
+        {
+            a -= Time.deltaTime  * (1/ lifeTime);
+            logo.color = new Color(logo.color.r, logo.color.g, logo.color.b,a);
+        }
+    }
 }
